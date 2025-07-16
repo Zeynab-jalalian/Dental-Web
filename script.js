208f8b2e0a85d32925192cbbbcff9792f59f0726
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-07-16 12:13:32 +0330

    validation

--- script.js
+++ script.js
@@ -1,9 +1,64 @@
-const hamMenu=document.querySelector(".menu-toggle"),
-      hamIcon=document.querySelector(".menu-toggle i"),
-      menu=document.querySelector(".menu-ul");
+/* menu */
+const hamMenu = document.querySelector(".menu-toggle"),
+  hamIcon = document.querySelector(".menu-toggle i"),
+  menu = document.querySelector(".menu-ul");
 
-hamMenu.addEventListener("click",()=>{
-    hamIcon.classList.toggle("fa-times");
-    menu.classList.toggle("active");
-    
-})
+hamMenu.addEventListener("click", () => {
+  hamIcon.classList.toggle("fa-times");
+  menu.classList.toggle("active");
+});
+/* form */
+const formBtn = document.querySelector(".formBtn");
+
+formBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const textInputEl = document.querySelector("#inputText");
+  const numberInputEl = document.querySelector("#inputNumber");
+
+  const textInput = textInputEl.value.trim();
+  const numberInput = numberInputEl.value.trim();
+
+  const nameRegex = /^[\u0600-\u06FF\s]{3,}$/;
+  const phoneRegex = /^0[1-9][0-9]{9}$/;
+
+  if (textInput === "") {
+    document.querySelector(".text").innerHTML = `
+      <i class="fas fa-exclamation-triangle"></i>
+      لطفاً نام و نام خانوادگی را وارد کنید.
+    `;
+    return;
+  }
+
+  if (numberInput === "") {
+    document.querySelector(".text").innerHTML = `
+      <i class="fas fa-exclamation-triangle"></i>
+      لطفاً شماره تماس را وارد کنید.
+    `;
+    return;
+  }
+
+  if (!nameRegex.test(textInput)) {
+    document.querySelector(".text").innerHTML = `
+      <i class="fas fa-exclamation-triangle"></i>
+      لطفا نام و نام خانوادگی خود را به درستی وارد نمایید.
+    `;
+    return;
+  }
+
+  if (!phoneRegex.test(numberInput)) {
+    document.querySelector(".text").innerHTML = `
+      <i class="fas fa-exclamation-triangle"></i>
+      لطفا شماره موبایل خود را به درستی وارد نمایید.
+    `;
+    return;
+  }
+
+  document.querySelector(".text").innerHTML = `
+    <i class="fas fa-check"></i>
+    ممنون :) مشاورین ما به زودی با شما تماس می‌گیرند.
+  `;
+
+  textInputEl.value = "";
+  numberInputEl.value = "";
+});
